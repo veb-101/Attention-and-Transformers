c23857573879d0bfcb78894f23564464a204c8cf
Vaibhav Singh <vaibhav.singh.3001@gmail.com>
2022-12-13 01:30:25 +0530

    fixed unfolding & folding in MBViT block

--- Attention_and_Transformers/MobileViT_v1/mobile_vit_v1_block.py
+++ Attention_and_Transformers/MobileViT_v1/mobile_vit_v1_block.py
@@ -52,6 +52,69 @@ class Transformer(Layer):
         return x
 
 
+# https://github.com/apple/ml-cvnets/blob/84d992f413e52c0468f86d23196efd9dad885e6f/cvnets/modules/mobilevit_block.py#L186
+def unfolding(nn, patch_h=2, patch_w=2):
+    """
+    H, W
+    [                            [
+        [1, 2, 3, 4],     Goal      [1, 3, 9, 11],
+        [5, 6, 7, 8],     ====>     [2, 4, 10, 12],
+        [9, 10, 11, 12],            [5, 7, 13, 15],
+        [13, 14, 15, 16],           [6, 8, 14, 16]
+    ]                            ]
+    """
+    B, H, W, C = tf.shape(nn)[0], tf.shape(nn)[1], tf.shape(nn)[2], tf.shape(nn)[3]
+    patch_area = int(patch_h * patch_w)
+
+    num_patch_h, num_patch_w = int(tf.math.ceil(H / patch_h)), int(tf.math.ceil(W / patch_w))
+    num_patches = num_patch_h * num_patch_w
+
+    interpolate = False
+
+    if ((num_patch_h * patch_h) != H) or ((num_patch_w * patch_w) != W):
+        nn = tf.image.resize(nn, [num_patch_h * patch_h, num_patch_w * patch_w], method="bilinear")
+        interpolate = True
+
+    reshaped_fm = tf.reshape(nn, (B * num_patch_h, patch_h, num_patch_w, patch_w * C))
+    transposed_fm = tf.transpose(reshaped_fm, perm=[0, 2, 1, 3])
+    reshaped_fm = tf.reshape(transposed_fm, (B, num_patches, patch_area, C))
+    transposed_fm = tf.transpose(reshaped_fm, perm=[0, 2, 1, 3])
+
+    info_dict = {
+        "orig_size": (H, W),
+        "interpolate": interpolate,
+        "num_patches_w": num_patch_w,
+        "num_patches_h": num_patch_h,
+    }
+
+    return transposed_fm, info_dict
+
+
+# https://github.com/apple/ml-cvnets/blob/84d992f413e52c0468f86d23196efd9dad885e6f/cvnets/modules/mobilevit_block.py#L233
+def folding(nn, info_dict: dict, patch_h=2, patch_w=2):
+
+    B, C = tf.shape(nn)[0], tf.shape(nn)[3]
+
+    num_patch_h = info_dict["num_patches_h"]
+    num_patch_w = info_dict["num_patches_w"]
+
+    # [B, P, N C] --> [B, N, P, C]
+    nn = tf.transpose(nn, perm=(0, 2, 1, 3))
+
+    # [B, N, P, C] --> [B*nh, nw, ph, pw*C]
+    nn = tf.reshape(nn, (B * num_patch_h, num_patch_w, patch_h, patch_w * C))
+
+    # [B*nh, nw, ph, pw*C] --> [B*nh, ph, nw, pw*C]
+    nn = tf.transpose(nn, perm=(0, 2, 1, 3))
+
+    # [B*nh, ph, nw, pw*C] --> [B, nh*ph, nw,pw, C]
+    nn = tf.reshape(nn, (B, num_patch_h * patch_h, num_patch_w * patch_w, C))
+
+    if info_dict["interpolate"]:
+        nn = tf.image.resize(nn, size=info_dict["orig_size"])
+    return nn
+
+
 class MobileViTBlock(Layer):
     def __init__(
         self,
@@ -78,19 +141,20 @@ class MobileViTBlock(Layer):
             num_filters=self.embedding_dim, kernel_size=1, strides=1, use_bn=False, use_activation=False, use_bias=False
         )
 
+        layers = [
+            Transformer(
+                embedding_dim=self.embedding_dim,
+                num_heads=self.num_heads,
+                linear_drop=linear_drop,
+                attention_drop=attention_drop,
+            )
+            for _ in range(self.transformer_repeats)
+        ]
+
+        layers.append(LayerNormalization(epsilon=1e-6))
+
         # Repeated transformer blocks
-        self.transformer_blocks = Sequential(
-            layers=[
-                Transformer(
-                    embedding_dim=self.embedding_dim,
-                    num_heads=self.num_heads,
-                    linear_drop=linear_drop,
-                    attention_drop=attention_drop,
-                )
-                for _ in range(self.transformer_repeats)
-            ]
-        )
-        self.norm = LayerNormalization(epsilon=1e-6)
+        self.transformer_blocks = Sequential(layers=layers)
 
         # Fusion blocks
         self.local_features_3 = ConvLayer(num_filters=self.out_filters, kernel_size=1, strides=1, use_bn=True, use_activation=True)
@@ -98,24 +162,19 @@ class MobileViTBlock(Layer):
         self.fuse_local_global = ConvLayer(num_filters=self.out_filters, kernel_size=3, strides=1, use_bn=True, use_activation=True)
 
     def call(self, x):
-        B, H, W = tf.shape(x)[0], tf.shape(x)[1], tf.shape(x)[2]
-
         local_representation = self.local_features_1(x)
         local_representation = self.local_features_2(local_representation)
-
-        # num_patches = tf.cast((H * W) / self.patch_size, tf.int32)
 
         # Transformer as Convolution Steps
         # --------------------------------
         # # Unfolding
-        unfolded = tf.reshape(local_representation, (B, self.patch_size, -1, self.embedding_dim))
+        unfolded, info_dict = unfolding(local_representation)
 
         # # Infomation sharing/mixing --> global representation
         global_representation = self.transformer_blocks(unfolded)
-        global_representation = self.norm(global_representation)
 
         # # Folding
-        folded = tf.reshape(global_representation, (B, H, W, self.embedding_dim))
+        folded = folding(global_representation, info_dict=info_dict)
         # # --------------------------------
 
         # Fusion
